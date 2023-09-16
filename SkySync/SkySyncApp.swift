60649116856aefd71abf0e08f2354dea789c45be
12944qwerty <qwerty12944qwerty@gmail.com>
2023-09-16 10:47:28 -0400

    Initial Commit

---
+++ SkySync/SkySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkySyncApp.swift
+//  SkySync
+//
+//  Created by Krish Shah on 9/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SkySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
